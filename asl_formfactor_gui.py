808657e14bd57bf7f1d4ab070fc93ddeb9befd9d
cmalamug <ciara@malamug.com>
2025-03-18 13:17:08 -0400

    debug space bar input

--- asl_formfactor_gui.py
+++ asl_formfactor_gui.py
@@ -5,6 +5,7 @@ texts = ["Hello, World!", "Press Space to Change", "Tkinter is fun!", "Python GU
 current_index = 0  # Track the current text index
 
 def change_text(event=None):
+    print(f"Key pressed: {event.keysym}") 
     global current_index
     current_index = (current_index + 1) % len(texts)  # Cycle through texts
     label.config(text=texts[current_index])
@@ -20,6 +21,7 @@ label.pack(expand=True)
 
 # Bind spacebar to change text
 root.bind("<space>", change_text)
+root.focus_set()
 
 # Run the Tkinter event loop
 root.mainloop()
